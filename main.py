30858e5730b5271bddfff61a9946c6d34b2c1a15
Aniruddha Chaudhary <anirudh4000@gmail.com>
2022-11-08 19:50:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import *
+import json, time
+import scalar
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set ={'Page': 'Home', 'Message': 'Successfully loaded the home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+@app.route('/scale/', methods=['GET'])
+def request_page():
+
+    user_query = str(request.args.get('values')) # /scale/?values=0.10/0.2/1/100/2/1/0/1/0/1/0/1/0/1/1/1/0/1
+
+    ndarray = scalar.transform(user_query)
+
+
+
+
+
+    data_set ={'value_one': f'{ndarray[0][0]}', 'value_two': f'{ndarray[0][1]}', 
+                'value_three': f'{ndarray[0][2]}', 'value_four': f'{ndarray[0][3]}', 
+                'value_five': f'{ndarray[0][4]}', 'value_six': f'{ndarray[0][5]}', 
+                'value_seven': f'{ndarray[0][6]}', 'value_eight': f'{ndarray[0][7]}', 
+                'value_nine': f'{ndarray[0][8]}', 'value_ten': f'{ndarray[0][9]}', 
+                'value_eleven': f'{ndarray[0][10]}', 'value_twelve': f'{ndarray[0][11]}', 
+                'value_thirteen': f'{ndarray[0][12]}', 'value_fourteen': f'{ndarray[0][13]}', 
+                'value_fifteen': f'{ndarray[0][14]}', 'value_sixteen': f'{ndarray[0][15]}', 
+                'value_seventeen': f'{ndarray[0][16]}', 'value_eighteen': f'{ndarray[0][17]}'}
+    
+
+    json_dump = jsonify(data_set)
+    json_dump.headers.add('Access-Control-Allow-Origin', '*')
+
+    return json_dump
+
+if __name__ == '__main__':
+    app.run(port=7777)
---
+++ scalar.py
@@ -0,0 +1,49 @@
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv("HR_comma_sep.csv")
+
+
+feats = ['department','salary']
+df_final = pd.get_dummies(df,columns=feats,drop_first=True)
+
+
+from sklearn.model_selection import train_test_split
+
+
+X = df_final.drop(['left'],axis=1).values
+y = df_final['left'].values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+def transform(str):
+    
+
+    list = str.split("/")
+
+    for idx, x in enumerate(list):
+        list[idx] = float(x)
+
+    
+    return sc.transform(np.array([[list[0],list[1] ,list[2], list[3], list[4], list[5],list[6],list[7],list[8], list[9],list[10],list[11],list[12],list[13],list[14],list[15], list[16],list[17]]]))
+
+    # print(ndarray[0][1])
+
+
+
+
+
+
